3e9e4385cc054f8c9a0ffdd7a05a7b253b5af594
gypsydiver <fermendozarcs@gmail.com>
2018-07-12 16:23:25 +0200

    WIP: encoder

--- main.go
+++ main.go
@@ -14,33 +14,28 @@ import (
 // echo c29tZS1iYXNlLTY0 | b64 -d
 // echo YzI5dFpTMWlZWE5sTFRZMA== | b64 -d -n 2
 // echo some-base64 | b64
-// b64 -d -in in.txt -out out.txt
-// b64 -in in.txt -out out.txt
+// b64 -d
+// b64
 
 var (
-	n   = flag.Int("n", 1, "number of iterations to be performed")
-	v   = flag.Bool("v", false, "verbose, prints iterations")
-	d   = flag.Bool("d", false, "decode")
-	in  = flag.String("in", "", "input file")
-	out = flag.String("out", "", "output file")
+	n = flag.Int("n", 1, "number of iterations to be performed")
+	v = flag.Bool("v", false, "verbose, prints iterations")
+	d = flag.Bool("d", false, "decode")
 )
 
 func main() {
 	flag.Parse()
 
-	if *in != "" {
-		//open file
-	}
-	if *out != "" {
-		//open file
-	}
-
 	if *d {
-		reader := decode(os.Stdin, *n) // os.Stdin || or opened file
+		reader := decode(os.Stdin, *n)
 		all, _ := ioutil.ReadAll(reader)
 		fmt.Println(string(all))
 	} else {
-		// encode()
+		out := encode(os.Stdout, *n)
+		defer out.Close()
+
+		clear, _ := ioutil.ReadAll(os.Stdin)
+		out.Write(clear)
 	}
 }
 
@@ -61,4 +56,14 @@ func progress(r io.Reader) io.Reader {
 		fmt.Println(s)
 	}
 	return dst
+}
+
+func encode(r io.WriteCloser, n int) io.WriteCloser {
+	if n == 0 {
+		return r
+	}
+	if *v {
+
+	}
+	return encode(base64.NewEncoder(base64.StdEncoding, r), n-1)
 }
